6c871101bb05cafe408a5e0beadba3b8ef517707
Stikkerrr <storento@yandex.ru>
2021-01-16 01:07:27 +0300

    9.2. Checkstyle [#450170]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,16 +2,15 @@ package ru.job4j.condition;
 
 import java.util.Calendar;
 
-public class Greeting
-{
-    public static void main(String[] args)
-    {
+public class Greeting {
+
+    public static void main(String[] args) {
+
         String idea = "I like Java!";
 
         System.out.println(idea);
 
         idea += " But I am a newbie. ";
-
         int year = Calendar.getInstance().get(Calendar.YEAR);
         idea += year;
 
