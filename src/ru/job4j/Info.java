3c1e6933400ae4925784c22c8d41fe010f80fa8d
Stikkerrr <storento@yandex.ru>
2021-01-15 17:53:04 +0300

    3.4. Заголовок в файле java [#450175]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Info
+{
+    public static void main(String[] args)
+    {
+        Calendar   calendar   = new GregorianCalendar();
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+        System.out.println(dateFormat.format(calendar.getTime()));
+    }
+}
