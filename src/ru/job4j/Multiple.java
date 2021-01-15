fb836243fe42355217c340262f833c74ba8b55f7
Stikkerrr <storento@yandex.ru>
2021-01-15 17:31:30 +0300

    3.3. Разбор первой программы. [#450174]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Multiple
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            System.out.println("1*" + i + "=" + i);
+        }
+    }
+}
