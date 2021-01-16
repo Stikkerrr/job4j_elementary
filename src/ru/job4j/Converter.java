4621c51c4cb4140ea0375acc89a02bc2b8320d8b
Stikkerrr <storento@yandex.ru>
2021-01-16 19:10:36 +0300

    0. Что такое тестирование [#450186]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -1,30 +1,55 @@
 package ru.job4j;
 
-public class Converter
-{
-    public static int rubleToEuro(int value)
-    {
+public class Converter {
+
+    public static int rubleToEuro(int value) {
+
         return value / 70;
     }
 
-    public static int rubleToDollar(int value)
-    {
+    public static int rubleToDollar(int value) {
+
         return value / 60;
     }
 
-    public static int dollarToRuble(int value)
-    {
+    public static int dollarToRuble(int value) {
+
         return value * 60;
     }
 
-    public static int euroToRuble(int value)
-    {
+    public static int euroToRuble(int value) {
+
         return value * 70;
     }
 
-    public static void main(String[] args)
-    {
-        int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
+    public static void main(String[] args) {
+
+        boolean passed;
+        int     in            = 420;
+        int[]   expectedValue = new int[]{6, 7, 25200, 29400};
+
+        for (int i = 0; i < 4; i++) {
+
+            switch (expectedValue[i]) {
+
+                case 6:
+                    passed = expectedValue[i] == Converter.rubleToEuro(in);
+                    System.out.println("Method rubleToEuro. Test result: " + passed);
+                    break;
+                case 7:
+                    passed = expectedValue[i] == Converter.rubleToDollar(in);
+                    System.out.println("Method rubleToDollar. Test result: " + passed);
+                    break;
+                case 25200:
+                    passed = expectedValue[i] == Converter.dollarToRuble(in);
+                    System.out.println("Method dollarToRuble. Test result: " + passed);
+                    break;
+                case 29400:
+                    passed = expectedValue[i] == Converter.euroToRuble(in);
+                    System.out.println("Method euroToRuble. Test result: " + passed);
+                    break;
+                default:
+            }
+        }
     }
 }
