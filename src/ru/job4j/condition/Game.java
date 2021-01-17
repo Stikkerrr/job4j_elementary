0990df0ec75cd0f7f88545810fea0afa215b4a28
Stikkerrr <storento@yandex.ru>
2021-01-17 23:26:52 +0300

    4.1.0. Оператор if с блоком else [#450101] without spaces and with my comments

--- src/ru/job4j/condition/Game.java
+++ src/ru/job4j/condition/Game.java
@@ -2,24 +2,24 @@ package ru.job4j.condition;
 
 public class Game {
 	
+	/**
+	 * The menu method starts the game,
+	 * the name of which is passed in the parameter
+	 * @param name of the game
+	 */
 	public static void menu(String name) {
-		
-		if (name.equals("super mario")) { /* сравниваем переменную name с "super mario" */
-			
+		if (name.equals("super mario")) {
 			System.out.println("Start - super mario");
 		}
-		if (name.equals("tanks")) { /* Сравнить переменную name с "tanks" */
-			
+		if (name.equals("tanks")) {
 			System.out.println("Start - tanks");
 		}
-		if (name.equals("tetris")) { /* Сравнить переменную name с "tetris" */
-			
+		if (name.equals("tetris")) {
 			System.out.println("Start - tetris");
 		}
 	}
 	
 	public static void main(String[] args) {
-		
 		Game.menu("tanks");
 	}
 }
