c86e472dcfcc2fb4ed9e6c9a67013b3f84b67157
Stikkerrr <storento@yandex.ru>
2021-01-19 22:27:23 +0300

    5.4. Протеиновая диета [#450211]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class Fitness {
+	
+	/**
+	 * The method calculates the time it takes for Ivan to catch up with Nick in the lifted weight.
+	 *
+	 * @param ivan weight lifted by Ivan at the start
+	 * @param nik  weight lifted by Nick at the start
+	 */
+	public static int calc(int ivan, int nik) {
+		int month = 0;
+		while (ivan < nik) {
+			ivan *= 3;
+			nik *= 2;
+			month++;
+		}
+		return month;
+	}
+}
