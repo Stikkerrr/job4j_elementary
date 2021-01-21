7c6c3834d62a462cc7ab8f37f755b2fe12e58c2d
Stikkerrr <storento@yandex.ru>
2021-01-21 14:31:41 +0300

    6.5. Поиск индекса в диапазоне. [#450128]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -19,4 +19,24 @@ public class FindLoop {
 		}
 		return rst;
 	}
+	
+	/**
+	 * Brute force search method with limited range
+	 *
+	 * @param data   the array in which we are looking for data
+	 * @param el     the data we are looking for
+	 * @param start  search index
+	 * @param finish search index
+	 * @return index of the found value
+	 */
+	public static int indexOf(int[] data, int el, int start, int finish) {
+		int rst = -1;
+		for (int index = start; index < finish; index++) {
+			if (data[index] == el) {
+				rst = index;
+				break;
+			}
+		}
+		return rst;
+	}
 }
